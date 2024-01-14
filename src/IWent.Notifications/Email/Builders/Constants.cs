1b295b4a1845c132ebcfb8a6a8dae8d429b8a955
Vadzim Kurdzesau <VKurdzesau@slb.com>
2024-01-14 23:23:16 +0100

    fix: fix path on azure

--- src/IWent.Notifications/Email/Builders/Constants.cs
+++ src/IWent.Notifications/Email/Builders/Constants.cs
@@ -1,10 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace IWent.Notifications.Email.Builders;
 
 internal static class Constants
 {
-    private static string _resourceFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Resources");
+    private static string _resourceFolderPath = Path.Combine(Environment.CurrentDirectory, "Resources");
 
     public static string ResourceFolderPath => _resourceFolderPath;
 }
