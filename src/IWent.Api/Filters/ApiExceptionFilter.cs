9632f336308090d6c900eea121ebc21d5f429d99
Vadzim Kurdzesau <VKurdzesau@slb.com>
2024-01-14 20:38:14 +0100

    feat: added functions gitactions

--- src/IWent.Api/Filters/ApiExceptionFilter.cs
+++ src/IWent.Api/Filters/ApiExceptionFilter.cs
@@ -2,11 +2,19 @@
 using Microsoft.AspNetCore.Mvc;
 using IWent.Services.Exceptions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace IWent.Api.Filters;
 
 public class ApiExceptionFilter : IActionFilter, IOrderedFilter
 {
+    private readonly ILogger<ApiExceptionFilter> _logger;
+
+    public ApiExceptionFilter(ILogger<ApiExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
     public int Order => int.MaxValue - 10;
 
     public void OnActionExecuting(ActionExecutingContext context)
@@ -31,6 +39,7 @@ public class ApiExceptionFilter : IActionFilter, IOrderedFilter
         }
         else if (context.Exception is not ApiException)
         {
+            _logger.LogError(context.Exception, "An internal exception was thrown.");
             context.Result = new ObjectResult("An internal error occured.")
             {
                 StatusCode = StatusCodes.Status500InternalServerError
